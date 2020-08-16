ce8f6f336c488134ecb7c6fe2ec8fa67cb5c2576
Aqib_Iqbal <aqibiqbal48@gmail.com>
2020-08-16 15:25:15 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from './style';
+import { TopBar } from './Components/TopBar';
+import { Header } from './Components/Header';
+import { Services } from './Components/Services';
+import { Reviews } from './Components/Reviews';
+import { Contact } from './Components/Contact';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    return (
+        <div>
+            
+            <ThemeProvider theme={theme}>
+                <TopBar />
+                <main style={{ overflow: "auto" }}>
+                    <Header />
+                    <Services />
+                    <Reviews />
+                    <Contact />
+                </main>
+            </ThemeProvider>
+        </div>
+    )
 }
-
-export default App;
